fcf7d1b667c846a244c6f40cac0f7573cf89e056
Han-Sheng Wang <h.wang0520221@arts.ac.uk>
2023-02-01 12:18:56 +0000

    drinking game start

--- p-01/sketch.js
+++ p-01/sketch.js
@@ -1,8 +1,33 @@
+let greeting = []
+let currentNumber = 0;
+
+
+
 function setup() {
   createCanvas(480, 720);
 }
 
 function draw() {
-  background(220);
-  text("Hey, today is a lovely day!",100,100)
+  background(0);
+  fill(255);
+  textSize(30);
+  text('Drinking Game',100,30);
+  textSize(150);
+  text(currentNumber,100,200)
+
+  textSize(50);
+  switch(currentNumber){
+    case 1:
+      text('Pick sb to drink',50,300)
+    break;
+  }
+
+
+
+
+}
+
+function mousePressed(){
+  currentNumber = int(random(1,13));
+  console.log(currentNumber);
 }
