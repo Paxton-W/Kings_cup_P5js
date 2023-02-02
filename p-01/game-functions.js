f9f27b2ae6f44c3ef77f7fd54839ce0786752544
Han-Sheng Wang <h.wang0520221@arts.ac.uk>
2023-02-02 18:37:46 +0000

    Update game-functions.js

--- p-01/game-functions.js
+++ p-01/game-functions.js
@@ -2,24 +2,17 @@ let currentNumber = 0;
 let currentText;
 let bg_clr = {r:14,g:93,b:124}
 
-
-
-
-
 let desc_wd_x = 50;
 let desc_wd_y = 300;
 
 let never_current, categories_current;
 let king_cup_times = 0;
 
-
 let if_case_6 = false;
 let if_case_13 = false;
 let if_new_change = false;
 let friends_drink = [];
 let name1, name2;
-
-
 
 let set_delays = -70;
 
@@ -32,15 +25,15 @@ function background_control(){
 
 function draw_credit(){
   push()
-  fill(255,100);
-  textSize(10);
-  text('Drinking Game',10,height-4);
-  textSize(10);
-  text('By Paxton',300,height-4);
-  text('ver.1.0',350,height-4);
-  text('King : '+king_cup_times,width/2,height-4)
-  textAlign(CENTER);
-  textSize(25);
+    fill(255,100);
+    textSize(10);
+    text('Drinking Game',10,height-4);
+    textSize(10);
+    text('By Paxton',300,height-4);
+    text('ver.1.0',350,height-4);
+    text('King : '+king_cup_times,width/2,height-4)
+    textAlign(CENTER);
+    textSize(25);
   pop()
 }
 
@@ -52,7 +45,6 @@ function draw_center_items(){
     ellipse(width/2,height/2,220);
   pop()
 
-  
   //draw the center king sign
   push()
     fill(255,70);
@@ -91,63 +83,62 @@ function draw_drink_mate_lines(){
   fill(255);
   for(let i = 0;i<friends_drink.length-1;i+=2){
     push()
-    fill(244, 127, 48)
-    textStyle(BOLD)
-    textSize(20);
-    text(friends_drink[i+1],40,height-80+i*9);
-    textStyle(ITALIC)
-    textSize(15);
-    fill(237, 164, 116)
-    text(" needs to drink when ",60+friends_drink[i+1].length*11,height-80+i*9);
-    textStyle(BOLD)
-    textSize(20);
-    fill(244, 127, 48)
-    text(friends_drink[i],60+150+friends_drink[i+1].length*11,height-80+i*9);
-    textStyle(ITALIC)
-    textSize(15);
-    fill(237, 164, 116)
-    text("drink",80+150+friends_drink[i+1].length*11+friends_drink[i].length*11,height-80+i*9);
+        fill(244, 127, 48)
+        textStyle(BOLD)
+        textSize(20);
+        text(friends_drink[i+1],40,height-80+i*9);
+        textStyle(ITALIC)
+        textSize(15);
+        fill(237, 164, 116)
+        text(" needs to drink when ",60+friends_drink[i+1].length*11,height-80+i*9);
+        textStyle(BOLD)
+        textSize(20);
+        fill(244, 127, 48)
+        text(friends_drink[i],60+150+friends_drink[i+1].length*11,height-80+i*9);
+        textStyle(ITALIC)
+        textSize(15);
+        fill(237, 164, 116)
+        text("drink",80+150+friends_drink[i+1].length*11+friends_drink[i].length*11,height-80+i*9);
     pop()
     push()
-    rotate(PI)
-    
-    fill(244, 127, 48)
-    textStyle(BOLD)
-    textSize(20);
-    text(friends_drink[i+1],-width+40,-80+i*9);
-    textStyle(ITALIC)
-    textSize(15);
-    fill(237, 164, 116)
-    text(" needs to drink when ",-width+60+friends_drink[i+1].length*11,-80+i*9);
-    textStyle(BOLD)
-    textSize(20);
-    fill(244, 127, 48)
-    text(friends_drink[i],-width+60+150+friends_drink[i+1].length*11,-80+i*9);
-    textStyle(ITALIC)
-    textSize(15);
-    fill(237, 164, 116)
-    text("drink",-width+80+150+friends_drink[i+1].length*11+friends_drink[i].length*11,-80+i*9);
+        rotate(PI)
+        fill(244, 127, 48)
+        textStyle(BOLD)
+        textSize(20);
+        text(friends_drink[i+1],-width+40,-80+i*9);
+        textStyle(ITALIC)
+        textSize(15);
+        fill(237, 164, 116)
+        text(" needs to drink when ",-width+60+friends_drink[i+1].length*11,-80+i*9);
+        textStyle(BOLD)
+        textSize(20);
+        fill(244, 127, 48)
+        text(friends_drink[i],-width+60+150+friends_drink[i+1].length*11,-80+i*9);
+        textStyle(ITALIC)
+        textSize(15);
+        fill(237, 164, 116)
+        text("drink",-width+80+150+friends_drink[i+1].length*11+friends_drink[i].length*11,-80+i*9);
     pop()
-    
   }
   pop();
 }
 
 function draw_current_task(){
   push()
-  fill(255);
-  textSize(30);
-  textAlign(CENTER);
-  text(currentText,width/2,height-100)
+    fill(255);
+    textSize(30);
+    textAlign(CENTER);
+    
+    text(currentText,width/2,height-100)
 
-  rotate(PI/2)
-  text(currentText,height/2,-50)
+    rotate(PI/2)
+    text(currentText,height/2,-50)
 
-  rotate(PI/2)
-  text(currentText,-width/2,-100)
+    rotate(PI/2)
+    text(currentText,-width/2,-100)
 
-  rotate(PI/2)
-  text(currentText,-height/2,width-50)
+    rotate(PI/2)
+    text(currentText,-height/2,width-50)
   pop()
 }
 
