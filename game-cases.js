ad3ef22d68e24dc2d4b214ca5bf766554609fb2d
Paxton W <102954952+Paxton-W@users.noreply.github.com>
2023-10-16 19:06:27 +0100

    Update game-cases.js

--- game-cases.js
+++ game-cases.js
@@ -16,67 +16,67 @@ function game_cases_switch() {
     case 3:
       currentText = "Three is me";
       if_new_change = false;
-      currentDis = "You drink"
+      currentDis = "You drink";
       center_e_clr = color(2, 110, 129);
       break;
     case 4:
       currentText = "Four is Girls";
-      currentDis = null
+      currentDis = null;
       if_new_change = false;
       center_e_clr = color(242, 102, 139);
       break;
     case 5:
       currentText = "Five is Guys";
-      currentDis = null
+      currentDis = null;
       if_new_change = false;
       center_e_clr = color(0, 153, 221);
       break;
     case 6:
       currentText = "Select\n a Mate";
-      currentDis = null
+      currentDis = null;
       if_case_6 = true;
       center_e_clr = color(4, 191, 157);
       break;
     case 7:
       currentText = "Heaven";
       if_new_change = false;
-      currentDis = "The last person put both hands \nin the air drinks"
+      currentDis = "The last person put both hands \nin the air drinks";
       center_e_clr = color(95, 205, 217);
       break;
     case 8:
       currentText = "Everyone";
       if_new_change = false;
       center_e_clr = color(166, 188, 9);
-      currentDis = "Cheers!!!"
+      currentDis = "Cheers!!!";
       break;
     case 9:
       currentText = "Rhyme Time";
       if_new_change = false;
       center_e_clr = color(140, 31, 40);
-      currentDis = "Think more than\n3 seconds drinks"
+      currentDis = "Think more than\n3 seconds drinks";
       break;
     case 10:
       currentText = "Categories";
       if_new_change = false;
-      currentDis = categories_current
+      currentDis = categories_current;
       center_e_clr = color(89, 28, 33);
       break;
     case 11:
       currentText = "Never Have \n I Ever";
       if_new_change = false;
-      currentDis = "n"
+      currentDis = null;
       center_e_clr = color(242, 147, 37);
       break;
     case 12:
       currentText = "Rule";
       if_new_change = false;
-      currentDis = "Make a rule, valid until next 12\n Anyone break the rule drinks"
+      currentDis = "Make a rule, valid until next 12\n Anyone break the rule drinks";
       center_e_clr = color(2, 82, 89);
       break;
     case 13:
       currentText = `King's Cup`;
       if_case_13 = true;
-      currentDis = `Pour your drink into king's cup`
+      currentDis = `Pour your drink into king's cup`;
       center_e_clr = color(242, 147, 37);
       break;
   }
